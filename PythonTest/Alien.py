db1060e2f938cf90e357fd8920b078a47664427f
jupadhyay1 <jupadhyay1@yahoo.com>
2020-01-30 21:43:09 -0800

    Small change

--- PythonTest/Alien.py
+++ PythonTest/Alien.py
@@ -19,10 +19,22 @@ class Alien(Sprite):
 def create_fleet(ai_settings,screen,aliens):
     alien=Alien(ai_settings,screen)
     alien_width=alien.rect.width
-    available_space_x=ai_settings.screen_width-2*alien_width
-    number_aliens_x=int(available_space_x/(2*alien_width))
+    
+    number_aliens_x=get_number_aliens_x(ai_settings,alien.rect.width)
     for alien_number in range(number_aliens_x):
+        create_alien(ai_settings,screen,aliens,alien_number)
+    
+def get_number_aliens_x(ai_settings,alien_width):
+    available_space_x=ai_settings.screen_width-2*alien_width
+    return int(available_space_x/(2*alien_width))
+
+def create_alien(ai_settings,screen,aliens,alien_number):
         alien=Alien(ai_settings,screen)
+        alien_width=alien.rect.width
         alien.x=alien_width +2*alien_width*alien_number
         alien.rect.x=alien.x
         aliens.add(alien)
+def get_number_rows(ai_settings,ship_height,alien_height):
+    available_space_y=(ai_settings.screen_height)-(3*alien_height)-ship_height
+    number_rows=int(available_space_y/(2*alien_height))
+    return number_rows
