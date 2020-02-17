8bcd8011f067796840119fd3349ac77084870a8e
jupadhyay1 <jupadhyay1@yahoo.com>
2020-02-17 14:02:32 -0800

    Error checking

--- DSA/LAB_2_UPADHYAY_JAY/LAB_2_UPADHYAY_JAY.cpp
+++ DSA/LAB_2_UPADHYAY_JAY/LAB_2_UPADHYAY_JAY.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
-float recPow(double , int&);
-int sumSquaresUp(int numInput);
+float recPow(double , int&);//Recursive exponenet power function
+int sumSquaresUp(int numInput);// Sum of squares 
 int sumSquaresDown(int numInput);
+const int MAXNUM = 10;
+const int MINNUM = -10;
+const int MINPOS = 0;
 using namespace std;
 int main()
 {
@@ -25,23 +28,33 @@ int main()
 				cin.ignore(1000, '\n');
 				cout << "Enter a base and exponent. Example: 1 3" << endl;
 				cin >> base >> exponent;
-				if (!cin)
-					cout << "Please reenter input. Must be numeric and within choices given." << endl;
+				if (!cin || base>MAXNUM || base<MINNUM||exponent >MAXNUM|| exponent<MINNUM)
+					cout << "Please reenter input. Two integers with a space between please. Must be in range of -10 to 10." << endl;
 			} while (!cin);
 			answer = recPow(base, exponent);
 			cout << answer;
 			break;
 		case 2:
-			
-			cout << "Enter numerical input" << endl;
-			cin>> numInput;
+			do {
+				cin.clear();
+				cin.ignore(1000, '\n');
+				cout << "Enter numerical input" << endl;
+				cin >> numInput;
+				if (!cin||numInput>MAXNUM||numInput<MINPOS)
+					cout << "Please reenter input. One integer within 1-10." << endl;
+			} while (!cin);
 			answer = sumSquaresUp(numInput);
 			cout << "=" <<  answer;
 			break;
 		case 3:
-			
+			do {
+				cin.clear();
+				cin.ignore(1000, '\n');
 			cout << "Enter numerical input" << endl;
 			cin >> numInput;
+			if (!cin || numInput > MAXNUM || numInput < MINPOS)
+				cout << "Please reenter input. One integer within 1-10." << endl;
+			} while (!cin);
 			answer = sumSquaresDown(numInput);
 			cout << "=" << answer;
 			break;
@@ -73,7 +86,7 @@ int sumSquaresUp(int numInput)
 	if (numInput == 1)
 	{
 		cout << "1";
-		return pow(1, 2);
+		return 1;
 	}
 	else
 	{
@@ -89,7 +102,7 @@ int sumSquaresDown(int numInput)
 	if (numInput == 1)
 	{
 		cout << "1";
-		return pow(1, 2);
+		return 1;
 	}
 	else
 	{
@@ -99,3 +112,41 @@ int sumSquaresDown(int numInput)
 		return (numInput * numInput) + answer;
 	}
 }
+
+
+
+/*// REGULAR INPUT
+
+1. Use power function
+2. Sum of Squares(low to high)
+3. Sum of Squares(high to low)
+4. End program
+1
+Enter a base and exponent. Example: 1 3
+2 3
+8
+1. Use power function
+2. Sum of Squares(low to high)
+3. Sum of Squares(high to low)
+4. End program
+2
+Enter numerical input
+5
+1+(2*2)+(3*3)+(4*4)+(5*5)=55
+1. Use power function
+2. Sum of Squares(low to high)
+3. Sum of Squares(high to low)
+4. End program
+3
+Enter numerical input
+5
+(5*5)+(4*4)+(3*3)+(2*2)+1=55
+1. Use power function
+2. Sum of Squares(low to high)
+3. Sum of Squares(high to low)
+4. End program
+4
+
+E:\SideProjects\DSA\LAB_2_UPADHYAY_JAY\Debug\LAB_2_UPADHYAY_JAY.exe (process 14232) exited with code 0.
+Press any key to close this window . . .
+*/
