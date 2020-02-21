3981a949742b22d7061ccda7c2881dee47198f8a
jupadhyay1 <jupadhyay1@yahoo.com>
2020-02-21 14:15:36 -0800

    Basic layout

---
+++ DSA/LAB_3_UPADHYAY_JAY/src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// Struct Record -- FirstName -- LastName-- BuildingNumber-- streetname-- city-- phone-- birthdate
+struct Record{
+    string firstName;
+    string lastName;
+    int numBuilding;
+    string streetName;
+    string cityName;
+    int numPhone;
+    int month;
+    int day;
+    int year;
+
+};
+// Struct Node ptr and record
+//AddressBook // pointer to head of list
+
+int main() {
+    std::cout << "Hello Easy C++ project!" << std::endl;
+}
