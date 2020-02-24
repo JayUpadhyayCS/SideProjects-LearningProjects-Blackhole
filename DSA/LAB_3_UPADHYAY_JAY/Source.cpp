2121710cde7d5ef85187e87b185dd9fb062272bb
jupadhyay1 <jupadhyay1@yahoo.com>
2020-02-23 20:08:05 -0800

    MENU

--- DSA/LAB_3_UPADHYAY_JAY/Source.cpp
+++ DSA/LAB_3_UPADHYAY_JAY/Source.cpp
@@ -3,11 +3,13 @@
 // ADDRESSBOOK pointer to a person
 //PERSON holds data
 // PersonList
-
+void clearCin();
+void strLower(string& tempStr);
 using namespace std;
 int main()
 {
 	int userChoice=1;
+	string userStr;
 	AddressBook recList;
 	cout << "Loading Addressbook, please hold.\n\n";
 	recList.Load();
@@ -19,13 +21,22 @@ int main()
 		switch (userChoice)
 		{
 		case 0:
-			cout << "Exitting program." << endl;
+			cout << "Exiting program." << endl;
 			break;
 		case 1:
 			recList.WriteFile();
 			break;
 		case 2:
-			recList.Search("string");
+
+			cout << "Enter a name or number youd like to search for.Enter Quit to return to main menu." << endl;
+			cin >> userStr;
+			clearCin();
+			strLower(userStr);
+			if (userStr != "Quit")
+			{
+				strLower(userStr);
+				recList.Search(userStr);
+			}
 			break;
 		case 3:
 		{
@@ -46,4 +57,17 @@ int main()
 		}
 	}
 	
+}
+void strLower(string& tempStr)
+{
+	tempStr.at(0) = toupper(tempStr.at(0));// What if they send empty string
+	for (int x = 1; x < tempStr.size(); x++)
+	{
+		tempStr.at(x) = tolower(tempStr.at(x));
+	}
+}
+void clearCin()
+{
+	cin.clear();
+	cin.ignore(1000, '\n');
 }
