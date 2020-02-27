a7a4c4991347702468958be54cc97d99526ae3a5
jupadhyay1 <jupadhyay1@yahoo.com>
2020-02-27 09:37:58 -0800

    Finished murphys lab

--- CS230/JXU_LAB1/JXULab1.cpp
+++ CS230/JXU_LAB1/JXULab1.cpp
@@ -4,15 +4,38 @@ using namespace std;
 
 int main()
 {
+	int maxDays[12]{ 31,28,31,30,31,30,31,31,30,31,30,31 };
 	short int data=0;
 	int year, month, day;
-	cout << "Enter month, day and year between 1-1-1980 and 12-31-2107" << endl;
-	// Sample output 3 20 2018 month day year In bit form it becomes year month day
-	cin >> month >> day >> year;
-	if (!cin)
+	bool reRun = true;
+	while (reRun)
 	{
-		cin.ignore(1000, '\n');
-		cin.clear();
+		reRun = false;
+		cout << "Enter month, day and year between 1 1 1980 and 12 31 2107 in following format, MM DD YYYY." << endl;
+		// Sample output 3 20 2018 month day year In bit form it becomes year month day
+		cin >> month >> day >> year;
+		if (!cin)
+		{
+			cin.ignore(1000, '\n');
+			cin.clear();
+			reRun = true;
+		}
+		if (month < 1 || month >12)
+		{
+			cout << "Error with month input." << endl;
+			reRun = true;
+		}
+		else if (day < 1 || day>maxDays[month])
+		{
+			cout << "Error with day input" << endl;
+			reRun = true;
+		}
+		else if (year < 1980 || year>2107)
+		{
+			cout << "Error with year input." << endl;
+			reRun = true;
+		}
+
 	}
 	// Year -- 7 bits
 	// Month-- 4 bits
@@ -26,16 +49,15 @@ int main()
 	year -=1980;
 	data = data | year;
 	data = data << 4;
-	cout << "After year operation: "<< data << endl;
 
 	data = data | month;
 	
-	cout << "After month operation: " << data << endl;
+
 	data = data << 5;
 	
 	data = data | day;
 	
 	
-	cout << hex << data << endl;
+	cout << "The hexadecimal FAT date is: " << hex << data << endl;
 
 }
