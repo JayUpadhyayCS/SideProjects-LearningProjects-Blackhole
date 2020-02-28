79ba738c85ff7d6c7b5e79ae3220d08e30b5bf4e
jupadhyay1 <jupadhyay1@yahoo.com>
2020-02-27 18:45:56 -0800

    Better structure

--- DSA/LAB_1_UPADHYAY_JAY/LAB_1_UPADHYAY_JAY.cpp
+++ DSA/LAB_1_UPADHYAY_JAY/LAB_1_UPADHYAY_JAY.cpp
@@ -1,4 +1,13 @@
 // Lab1 Upadhyay, Jay T TH  2/20/20
+// have something to write with
+//have cursor on top of program
+//zoom 150
+//print output file with all test data, main headers, implementations
+//dont leave it on the desk, get signature before turning it in.
+//leave it whre laptop is on gray table
+// write your name across on the program. Put T so she knows which class.
+
+
 #include <cctype>
 #include <string>
 #include <vector>
@@ -9,20 +18,20 @@ const int SPACE = 15;
 const int ARRSIZE = 100;
 using namespace std;
 
-struct InventoryItem{
+struct InventoryItem {
 	string uniqId;// item id
 	string itmName;// item name
 	int numItems;// quantity on hand
 	double itmPrice;// item price
 };
-class InventoryItems{
+class InventoryItems {
 private:
 	vector<InventoryItem> vecItms;// unsorted inventory tiems
 	vector<InventoryItem*> sortedItms;//pointers toward inventory items sorted some way.
 public:
-	bool InputRecords();// Input records into unsorted vec
+	void InputRecords();// Input records into unsorted vec
 	void UnsortPrint();//Print Unsorted vec
-	void BubSort(int userInput); // sort ptr to a field based on userinput
+	void BubSort(int); // sort ptr to a field based on userinput
 	void PrintSorted(); // print data sortedvec ptr pointing to
 	void SearchInv(string userInput); // Search unsorted vec for id/name
 	void PrintReport();// print uniqueitems// total price // total quantity 
@@ -30,24 +39,20 @@ public:
 string strLower(string tempStr);// Make string lowercase
 void clearCin(); // Clear cin stream
 void RunMenu(InventoryItems&);
-
+void RunSortCase(InventoryItems& itmList);
+void RunSearchCase(InventoryItems& itmList);
 int main()
 {
 	InventoryItems itmList;
-	bool errorFound = itmList.InputRecords();// Input datafile and check for errors
-	
-	if (errorFound)  
-		cout << "Error taking input. File may be missing or empty.\n"; 
-	else {// if no errors then continue
-		RunMenu(itmList);
-	}
+	itmList.InputRecords();// Input datafile and check for errors If error found, ends program.
+	RunMenu(itmList);
+	return 0;
 }
 
 
-void RunMenu(InventoryItems & itmList)// Requires inventory populated
+void RunMenu(InventoryItems& itmList)// Requires inventory populated
 {
 	int numInput;
-	string strInput;
 	bool runMenu = true;
 	while (runMenu)
 	{
@@ -60,37 +65,12 @@ void RunMenu(InventoryItems & itmList)// Requires inventory populated
 			itmList.UnsortPrint();
 			break;
 		case 2: // Print inventory sorted in ascending order by any field
-			while (numInput != 5) // 5 is the option to exit
-			{
-				cout << "What would you like to sort by?\n1:ID\n2:Name\n3:Quantity\n4:Price\n5:Back to Main Menu\n";
-				cin >> numInput;
-				if (numInput > 0 && numInput < 5)//check if it is  one of the options
-				{
-					itmList.BubSort(numInput);
-					itmList.PrintSorted();
-				}
-				else if (numInput == 5)
-				{
-					cout << "Returning to main menu" << endl;
-				}
-				else
-				{
-					cout << "Problem with input. Retry and enter a number.\n";
-					clearCin();
-				}
-			}
+			RunSortCase(itmList);
 			break;
 
 		case 3: // Search for item by ID or name
-			do {
-				cout << "\nEnter name or ID of an item you are searching for, or enter 1 to return to Main Menu.\n"; cin >> strInput;
-				clearCin();
-				if (strInput != "1")
-				{
-					strInput=strLower(strInput);
-					itmList.SearchInv(strInput);
-				}
-			} while (strInput != "1");
+			RunSearchCase(itmList);
+
 			break;
 		case 4: // Print report of number of unique items, and total count/worth of items
 			itmList.PrintReport();
@@ -98,60 +78,64 @@ void RunMenu(InventoryItems & itmList)// Requires inventory populated
 		case 5:// End program
 
 			cout << "Terminating program" << endl;
-			runMenu= false;
+			runMenu = false;
 			break;
-		default: cout << "Please retry. Could not understand input.\n"; // Input error, clear cin and retry
+		default:
+			cout << "Please retry. Could not understand input.\n"; // Input error, clear cin and retry
 			clearCin();
 		}
 	}
 }
-
-bool InventoryItems:: InputRecords() {
+void InventoryItems::InputRecords() {
 	ifstream inputStream;
-	inputStream.open("input.txt");
-	//inputStream.open("E:\\SideProjects\\DSA\\LAB_1_UPADHYAY_JAY\\input.txt");
-	if (!inputStream) 
-		return true;// error found
+	//inputStream.open("input.txt");
+	inputStream.open("E:\\SideProjects\\DSA\\LAB_1_UPADHYAY_JAY\\input.txt");
+	ofstream outStream;
+	outStream.open("E:\\SideProjects\\DSA\\LAB_1_UPADHYAY_JAY\\error.txt", ofstream::out);
+	if (!inputStream)
+	{
+		cout << "Error taking input.File may be missing or empty.Ending program.\n";
+		exit(-1);// Error Code-1
+	}
 	InventoryItem bufferItm;
 	while (!inputStream.eof() && vecItms.size() < ARRSIZE) {
 		inputStream >> bufferItm.uniqId >> bufferItm.itmName
 			>> bufferItm.numItems >> bufferItm.itmPrice;
-		if (!inputStream) 
-			return true;// error founds
-		if (bufferItm.numItems > 0 && bufferItm.itmPrice > 0)
+		if (bufferItm.numItems >= 0 && bufferItm.itmPrice >= 0)
 		{
-			bufferItm.itmName=strLower(bufferItm.itmName);
+			bufferItm.itmName = strLower(bufferItm.itmName);
 			vecItms.push_back(bufferItm);
+
 		}
 		else
 		{
-			cout << "Error with this piece of data" << bufferItm.uniqId << " " << bufferItm.itmName << " "
+			cout << "Error with this piece of data: " << bufferItm.uniqId << " " << bufferItm.itmName << " "
+				<< bufferItm.numItems << " " << fixed << showpoint << setprecision(2) << bufferItm.itmPrice << "\nSaving to error.txt\n";
+			outStream << "Error with this piece of data: " << bufferItm.uniqId << " " << bufferItm.itmName << " "
 				<< bufferItm.numItems << " " << fixed << showpoint << setprecision(2) << bufferItm.itmPrice << "\n";
 		}
 	}
 	if (vecItms.size() >= ARRSIZE)
 	{
-		cout << "There were more than "<<vecItms.size() <<" items in datafile. Stopped after data:\n" << bufferItm.uniqId << " " << bufferItm.itmName << " " 
-			<< bufferItm.numItems << " " <<fixed << showpoint << setprecision(2) << bufferItm.itmPrice << "\n";// Put buffer items details here
+		cout << "There were more than " << vecItms.size() << " items in datafile. Stopped after data:\n" << bufferItm.uniqId << " " << bufferItm.itmName << " "
+			<< bufferItm.numItems << " " << fixed << showpoint << setprecision(2) << bufferItm.itmPrice << "\n";// Put buffer items details here
 	}
 	inputStream.close();
 	for (int i = 0; i < vecItms.size(); i++)// Initialize array of pointers
 	{
 		sortedItms.push_back(&vecItms.at(i));
 	}
-	return false;// no errors1
 }
 void InventoryItems::UnsortPrint()
 {
-	cout << left << setw(SPACE) << "Item ID" << left << setw(SPACE) << "Item Name" << left << setw(SPACE) << "Quantity" << left << setw(SPACE) << "Price" << endl;
+	cout << left << setw(SPACE) << "Item ID" << left << setw(SPACE) << "Item Name" << right << setw(SPACE) << "Quantity" << right << setw(SPACE) << "Price" << endl;
 	for (int x = 0; x < vecItms.size(); x++)
 	{
-		cout << left << setw(SPACE) << vecItms.at(x).uniqId << left << setw(SPACE) << left << vecItms.at(x).itmName << setw(SPACE)
-			<< vecItms.at(x).numItems << left << setw(SPACE) <<fixed << showpoint << setprecision(2) << vecItms.at(x).itmPrice << endl;
+		cout << left << setw(SPACE) << vecItms.at(x).uniqId << left << setw(SPACE) << left << vecItms.at(x).itmName << right << setw(SPACE)
+			<< vecItms.at(x).numItems << right << setw(SPACE) << fixed << showpoint << setprecision(2) << vecItms.at(x).itmPrice << endl;
 	}
 }
-
-void InventoryItems::BubSort(int userChoice) {
+void InventoryItems::BubSort(int numInput) {
 	InventoryItem* temp;
 	bool isSwap = true;
 	bool repeatLoop = true;
@@ -159,46 +143,46 @@ void InventoryItems::BubSort(int userChoice) {
 	{
 		repeatLoop = false;// No error in data position yet
 		isSwap = false;
-		int vecSize = sortedItms.size()-1;
+		int vecSize = sortedItms.size() - 1;
 		for (int i = 0; i < vecSize; i++)
 		{
 			isSwap = false;
 			//Write one function, that can sort by any field using array of pointers. 
 			//Do not copy and paste sort code five times into the same function. Bubble sort  is the easiest to modify.
-			switch (userChoice)
+			switch (numInput)
 			{
 			case 1:
-				isSwap = sortedItms.at(i)->uniqId> sortedItms.at(i + 1)->uniqId;
+				isSwap = sortedItms.at(i)->uniqId > sortedItms.at(i + 1)->uniqId;
 				break;
 			case 2:
-				isSwap = sortedItms.at(i)->itmName>sortedItms.at(i + 1)->itmName;
+				isSwap = sortedItms.at(i)->itmName > sortedItms.at(i + 1)->itmName;
 				break;
 			case 3:
-				isSwap = sortedItms.at(i)->numItems> sortedItms.at(i + 1)->numItems;
+				isSwap = sortedItms.at(i)->numItems > sortedItms.at(i + 1)->numItems;
 				break;
 			case 4:
-				isSwap = sortedItms.at(i)->itmPrice> sortedItms.at(i + 1)->itmPrice;
+				isSwap = sortedItms.at(i)->itmPrice > sortedItms.at(i + 1)->itmPrice;
 				break;
 			}
 			if (isSwap)
 			{
-				repeatLoop=true;
+				repeatLoop = true;
 				temp = sortedItms.at(i);
 				sortedItms.at(i) = sortedItms.at(i + 1);
 				sortedItms.at(i + 1) = temp;
 			}
 		}
 	}
-	
+
 }
 void InventoryItems::PrintSorted()// Print array of pointer values. Requires pointers initialized and sorted.
 {
-	cout << left << setw(SPACE) << "Item ID" << left << setw(SPACE) << "Item Name" << left << setw(SPACE) << "Quantity" << left << setw(SPACE) << "Price" << endl;
+	cout << left << setw(SPACE) << "Item ID" << left << setw(SPACE) << "Item Name" << right << setw(SPACE) << "Quantity" << right << setw(SPACE) << "Price" << endl;
 	int vecSize = sortedItms.size();
 	for (int x = 0; x < vecSize; x++)
 	{
-		cout << left << setw(SPACE) << sortedItms.at(x)->uniqId << left << setw(SPACE) << left << sortedItms.at(x)->itmName << setw(SPACE) << sortedItms.at(x)->numItems 
-			<< left << setw(SPACE) << fixed << showpoint << setprecision(2)<< sortedItms.at(x)->itmPrice << endl;
+		cout << left << setw(SPACE) << sortedItms.at(x)->uniqId << left << setw(SPACE) << left << sortedItms.at(x)->itmName << right << setw(SPACE) << sortedItms.at(x)->numItems
+			<< right << setw(SPACE) << fixed << showpoint << setprecision(2) << sortedItms.at(x)->itmPrice << endl;
 	}
 }
 void InventoryItems::SearchInv(string userInput)// Requires an inventory to search and valid userInput
@@ -229,10 +213,9 @@ void InventoryItems::PrintReport()// Basic print records function // requires in
 		numTotPrice += vecItms[x].itmPrice * vecItms[x].numItems;
 		numTotAmnt += vecItms[x].numItems;
 	}
-	cout << "Number of unique items: " << vecItms.size() << "\nTotal Price of Current Inventory: $" <<fixed << showpoint << setprecision(2)<<  numTotPrice
-		<< "\nTotal Amount of Items in Inventory: "  <<  numTotAmnt << endl;
+	cout << "Number of unique items: " << vecItms.size() << "\nTotal Price of Current Inventory: $" << fixed << showpoint << setprecision(2) << numTotPrice
+		<< "\nTotal Amount of Items in Inventory: " << numTotAmnt << endl;
 }
-
 std::string strLower(std::string tempStr)
 {
 	if (!tempStr.size())
@@ -244,8 +227,202 @@ std::string strLower(std::string tempStr)
 	}
 	return tempStr;
 }
-void clearCin() 
+void clearCin()
 {
 	cin.clear();
 	cin.ignore(1000, '\n');
 }
+
+void RunSortCase(InventoryItems& itmList)
+{
+	int numInput;
+	bool reRun = true;
+	while (reRun)
+	{
+		reRun = false;
+		int numInput;
+		cout << "What would you like to sort by?\n1:ID\n2:Name\n3:Quantity\n4:Price\n5:Back to Main Menu\n";
+		cin >> numInput;
+		if (numInput > 0 && numInput < 5)//check if it is  one of the options
+		{
+			itmList.BubSort(numInput);
+			itmList.PrintSorted();
+		}
+		else if (numInput == 5)
+		{
+			cout << "Returning to main menu" << endl;
+			return;
+		}
+		else
+		{
+			cout << "Problem with input. Retry and enter a number.\n";
+			clearCin();
+			reRun = true;
+
+		}
+	}
+}
+
+void RunSearchCase(InventoryItems& itmList)
+{
+	string strInput = "";
+
+	
+	do
+	{
+		cout << "\nEnter name or ID of an item you are searching for, or enter 1 to return to Main Menu.\n";
+		cin >> strInput;
+		clearCin();
+		if (strInput != "1")
+		{
+			strInput = strLower(strInput);
+			itmList.SearchInv(strInput);
+		}
+		
+	} while (strInput != "1");
+}
+/*
+Error with this piece of data: 404 PeanutButter -2 2.00
+Saving to error.txt
+Error with this piece of data: 1404 Jelly 2 -2.00
+Saving to error.txt
+
+Enter a numeric choice that corresponds to the desired operation.
+1: Print Inventory Unsorted
+2: Print Inventory Sorted
+3. Search Record with ID or Name
+4. Print Totals Report
+5. End Program
+1
+Item ID        Item Name             Quantity          Price
+1228           Apple                        1           1.00
+9653           Orange                       9           1.50
+1338           Banana                       2           0.50
+9658           Bagels                       3           0.00
+1010           Bread                       15           0.15
+1991           Coke                        12           1.00
+92             Fanta                       12           1.00
+388            Granolabar                   1           1.50
+2828           Tissues                     50           0.05
+6767           Paper                     1000           0.10
+95             Milk                         0           5.00
+1000           Poptarts                     2           1.50
+
+Enter a numeric choice that corresponds to the desired operation.
+1: Print Inventory Unsorted
+2: Print Inventory Sorted
+3. Search Record with ID or Name
+4. Print Totals Report
+5. End Program
+2
+What would you like to sort by?
+1:ID
+2:Name
+3:Quantity
+4:Price
+5:Back to Main Menu
+1
+Item ID        Item Name             Quantity          Price
+1000           Poptarts                     2           1.50
+1010           Bread                       15           0.15
+1228           Apple                        1           1.00
+1338           Banana                       2           0.50
+1991           Coke                        12           1.00
+2828           Tissues                     50           0.05
+388            Granolabar                   1           1.50
+6767           Paper                     1000           0.10
+92             Fanta                       12           1.00
+95             Milk                         0           5.00
+9653           Orange                       9           1.50
+9658           Bagels                       3           0.00
+What would you like to sort by?
+1:ID
+2:Name
+3:Quantity
+4:Price
+5:Back to Main Menu
+3
+Item ID        Item Name             Quantity          Price
+95             Milk                         0           5.00
+1228           Apple                        1           1.00
+388            Granolabar                   1           1.50
+1000           Poptarts                     2           1.50
+1338           Banana                       2           0.50
+9658           Bagels                       3           0.00
+9653           Orange                       9           1.50
+1991           Coke                        12           1.00
+92             Fanta                       12           1.00
+1010           Bread                       15           0.15
+2828           Tissues                     50           0.05
+6767           Paper                     1000           0.10
+What would you like to sort by?
+1:ID
+2:Name
+3:Quantity
+4:Price
+5:Back to Main Menu
+4
+Item ID        Item Name             Quantity          Price
+9658           Bagels                       3           0.00
+2828           Tissues                     50           0.05
+6767           Paper                     1000           0.10
+1010           Bread                       15           0.15
+1338           Banana                       2           0.50
+1228           Apple                        1           1.00
+1991           Coke                        12           1.00
+92             Fanta                       12           1.00
+388            Granolabar                   1           1.50
+1000           Poptarts                     2           1.50
+9653           Orange                       9           1.50
+95             Milk                         0           5.00
+What would you like to sort by?
+1:ID
+2:Name
+3:Quantity
+4:Price
+5:Back to Main Menu
+5
+Returning to main menu
+
+Enter a numeric choice that corresponds to the desired operation.
+1: Print Inventory Unsorted
+2: Print Inventory Sorted
+3. Search Record with ID or Name
+4. Print Totals Report
+5. End Program
+3
+
+Enter name or ID of an item you are searching for, or enter 1 to return to Main Menu.
+hello
+Record not found. Sorry, Please retry with different data.
+Enter name or ID of an item you are searching for, or enter 1 to return to Main Menu.
+banana
+Record has been found!
+1338           Banana         2              0.50
+
+Enter name or ID of an item you are searching for, or enter 1 to return to Main Menu.
+1
+
+Enter a numeric choice that corresponds to the desired operation.
+1: Print Inventory Unsorted
+2: Print Inventory Sorted
+3. Search Record with ID or Name
+4. Print Totals Report
+5. End Program
+4
+Number of unique items: 12
+Total Price of Current Inventory: $148.75
+Total Amount of Items in Inventory: 1107
+
+Enter a numeric choice that corresponds to the desired operation.
+1: Print Inventory Unsorted
+2: Print Inventory Sorted
+3. Search Record with ID or Name
+4. Print Totals Report
+5. End Program
+5
+Terminating program
+
+c:\Users\student.CS\source\repos\Project1\Debug\Project1.exe (process 11364) exited with code 0.
+Press any key to close this window . . .
+*/
--- DSA/LAB_2_UPADHYAY_JAY/LAB_2_UPADHYAY_JAY.cpp
+++ DSA/LAB_2_UPADHYAY_JAY/LAB_2_UPADHYAY_JAY.cpp
@@ -2,27 +2,30 @@
 
 //Erro with neg number
 #include <iostream>
-const int MAXNUM = 10;// Maximum number for base, exponent, and sum of squares input
-const int MINNUM = -10;// Minumum number for base, exponent input
-const int MINPOS = 1;// Minimum input for sum of squares input
-float RecPower(double , int);//Recursive exponenet power function
+#include <stdlib.h>
+#include <string>
+double RecPower(double , int);//Recursive exponenet power function
 int SumSquaresUp(int numInput);// Sum of squares start low then ascend
 int SumSquaresDown(int numInput);// sum of squares, start max then descend
 void promptMenu();
-
+int GetNum();
+void RunPowerCase();
+void RunSquareUp();
+void RunSquareDown();
+void clearCin();
 using namespace std;
 int main()
 {
-	//No precondition.
+//No precondition.
 	promptMenu();
+	return 0;
 }
+
+
 void promptMenu()
 {
-	double numSolution;
-	int numInput;
 	int userInput;
-	bool continueProg = true;// Repeat menu until user wants out
-	bool rePrompt = true;// no errors caught so far
+	bool continueProg = true;
 	while (continueProg)
 	{
 		cout << "\n1. Use power function\n2. Sum of Squares(low to high)\n3. Sum of Squares(high to low)\n4. End program\n";
@@ -30,83 +33,50 @@ void promptMenu()
 		switch (userInput)
 		{
 		case 1:
-			double base;
-			int exponent;
-			rePrompt = true;
-			do
-			{
-				cin.clear();
-				cin.ignore(1000, '\n');
-				cout << "Enter a base and exponent. Example: 1 3. Press any character or 0 to go back to main menu." << endl;
-				cin >> base;
-				if (!base)// ASCII value of q
-				{
-					cout << "Exiting to main menu." << endl;
-					cin.clear();
-					cin.ignore(1000, '\n');
-					rePrompt = false;
-				}
-				else
-				{
-					cin >> exponent;
-					if (!cin || base > MAXNUM || base<MINNUM || exponent >MAXNUM || exponent < MINNUM)
-					{
-						cout << "Error with input. Two integers with a space between please. Must be in range of -10 to 10." << endl;
-						rePrompt = true;
-					}
-					else
-						cout << base << " to the power of " << exponent << " equals " << RecPower(base, exponent) << "!" << endl;// check to make sure base and exponent are valid
-				}
-			} while (rePrompt);// could use base instead but rePrompt is more understandable.
+			RunPowerCase();
 			break;
 		case 2:
+			RunSquareUp();
+			break;
 		case 3:
-			do {
-				rePrompt = true;
-				cin.clear();
-				cin.ignore(1000, '\n');
-				cout << "Enter numerical input 1-10 for the sum of squares.Otherwise please enter 0 to quit." << endl;
-				cin >> numInput;
-				if (!numInput)// ASCII value of q
-				{
-					cin.clear();
-					cin.ignore(1000, '\n');
-					cout << "Exiting to main menu." << endl;
-					rePrompt = false;
-				}
-				else if (!cin || numInput > MAXNUM || numInput < MINPOS)
-				{
-					cout << "Please reenter input. One integer within 1-10." << endl;
-				}
-				else if (userInput == 2)
-				{
-					numSolution = SumSquaresUp(numInput);// requires numInput to be >0 and initialized
-					cout << "=" << numSolution << endl;
-				}
-				else
-				{
-					numSolution = SumSquaresDown(numInput);// requires numInput to be >0 and initialized
-					cout << "=" << numSolution << endl;
-				}
-			} while (rePrompt);
+			RunSquareDown();
 			break;
-			//case 3:
-		case 4: // end program
-			continueProg = false;
-			break;
+		case 4: 
+			// end program//continueProg=false; or exit(0);
+			return;
 		default:
-			cout << "Putting you back into main menu. Could not understand input.\n";
-			cin.clear();
-			cin.ignore(1000, '\n');
+			clearCin();
 		}
 	}
 }
-float RecPower(double base, int exponent)// Recursive power function
+int GetNum() 
+{
+	int userInput;
+	string quitCheck;
+	do
+	{
+		cout << "Enter an amount of terms. Must be a positive integer >0 or enter Q to quit" << endl;
+		cin >> quitCheck;
+		if (quitCheck == "Q"|| quitCheck=="q")
+			return -1;
+		userInput = atoi(quitCheck.c_str());
+		if (userInput > 0)
+			return userInput;
+		else
+			cout <<"Error with input. Please retry" << endl;
+	}
+	while (!cin|| userInput <=0);
+}
+double RecPower(double base, int exponent)// Recursive power function
 {
 	if (exponent < 0)// If its negative exponent, I need to multiple 1/base against itself. Otherwise base*base;
 	{
 		exponent = -exponent;
 		base = 1 / base;
+	}
+	else if (exponent == 1)
+	{
+		return base;
 	}
 	else if (!exponent)
 		return 1;
@@ -143,85 +113,98 @@ int SumSquaresDown(int numInput)
 		return (numInput * numInput) + answer;
 	}
 }
+void RunPowerCase()
+{
+	double base;
+	int power;
+	bool error = false;
+	do {
+		error = false;
+		cout << "Enter base and power:  ";
+		cin >> base >> power;
 
+		if (!cin) {
+			cout << "Invalid input" << endl;
+			clearCin();
+			error = true;
+		}
+	} while (error);
+	cout << base << " to the power of " << power << " equals " << RecPower(base, power) << "!" << endl;
+}
+void RunSquareUp()
+{
+	int userInput;
+	int numSolution;
+	userInput = GetNum();
+	while (userInput != -1)
+	{
 
-
-/*// REGULAR INPUT
-
+		numSolution = SumSquaresUp(userInput);// requires numInput to be >0 and initialized
+		cout << "=" << numSolution << endl;
+		userInput = GetNum();
+	}
+}
+void RunSquareDown() 
+{
+	int userInput;
+	int numSolution;
+	userInput = GetNum();
+	while (userInput != -1)
+	{
+		numSolution = SumSquaresDown(userInput);// requires numInput to be >0 and initialized
+		cout << "=" << numSolution << endl;
+		userInput = GetNum();
+	}
+}
+void clearCin()
+{
+	if (!cin)
+	{
+		cout << "Error taking input, please try." << endl;
+		cin.clear();
+		cin.ignore(1000, '\n');
+	}
+}
+/*
 1. Use power function
 2. Sum of Squares(low to high)
 3. Sum of Squares(high to low)
 4. End program
 1
-Enter a base and exponent. Example: 1 3
+Please enter base and power in following format, if you want to quit to main menu, enter Q.
 2 3
-8
+2 to the power of 3 equals 8!
+Please enter base and power in following format, if you want to quit to main menu, enter Q.
+q
+
 1. Use power function
 2. Sum of Squares(low to high)
 3. Sum of Squares(high to low)
 4. End program
 2
-Enter numerical input
-5
-1+(2*2)+(3*3)+(4*4)+(5*5)=55
-1. Use power function
-2. Sum of Squares(low to high)
-3. Sum of Squares(high to low)
-4. End program
-3
-Enter numerical input
-5
-(5*5)+(4*4)+(3*3)+(2*2)+1=55
-1. Use power function
-2. Sum of Squares(low to high)
-3. Sum of Squares(high to low)
-4. End program
-4
-
-E:\SideProjects\DSA\LAB_2_UPADHYAY_JAY\Debug\LAB_2_UPADHYAY_JAY.exe (process 14232) exited with code 0.
-Press any key to close this window . . .
-
-////// Error input, testing bounds.
-
-1. Use power function
-2. Sum of Squares(low to high)
-3. Sum of Squares(high to low)
-4. End program
-0
-Putting you back into main menu. Could not understand input.
-
-1. Use power function
-2. Sum of Squares(low to high)
-3. Sum of Squares(high to low)
-4. End program
-1
-Enter a base and exponent. Example: 1 3
--11 2
-Please reenter input. Two integers with a space between please. Must be in range of -10 to 10.
-Enter a base and exponent. Example: 1 3
--2 -1
--0.5
-1. Use power function
-2. Sum of Squares(low to high)
-3. Sum of Squares(high to low)
-4. End program
-2
-Enter numerical input
-11
-Please reenter input. One integer within 1-10.
-Enter numerical input
-0
-Please reenter input. One integer within 1-10.
-Enter numerical input
+Enter an amount of terms. Must be a positive integer >0 or enter Q to quit
 3
 1+(2*2)+(3*3)=14
+Enter an amount of terms. Must be a positive integer >0 or enter Q to quit
+q
+
+1. Use power function
+2. Sum of Squares(low to high)
+3. Sum of Squares(high to low)
+4. End program
+3
+Enter an amount of terms. Must be a positive integer >0 or enter Q to quit
+3
+(3*3)+(2*2)+1=14
+Enter an amount of terms. Must be a positive integer >0 or enter Q to quit
+q
+
 1. Use power function
 2. Sum of Squares(low to high)
 3. Sum of Squares(high to low)
 4. End program
 4
 
-E:\SideProjects\DSA\LAB_2_UPADHYAY_JAY\Debug\LAB_2_UPADHYAY_JAY.exe (process 16272) exited with code 0.
+c:\Users\student.CS\source\repos\Project2\Debug\Project2.exe (process 13596) exited with code 0.
 Press any key to close this window . . .
-
 */
