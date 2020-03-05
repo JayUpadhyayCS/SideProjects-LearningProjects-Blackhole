7d4e29827a7a3b4b951617d16d20db89656c5ca1
jupadhyay1 <jupadhyay1@yahoo.com>
2020-03-04 17:06:46 -0800

    Clear function working, hot potato working

--- DSA/LAB_4_UPADHYAY_JAY/Source.cpp
+++ DSA/LAB_4_UPADHYAY_JAY/Source.cpp
@@ -13,7 +13,9 @@ using namespace std;
 struct Person
 {
 	string name;
+	int index;
 	Person* next;
+	Person* prev;
 };
 class PersonList
 {
@@ -22,6 +24,8 @@ public:
 	~PersonList();
 	void InputPpl(int numPpl);
 	void HotPotato(int numPass, int numPpl);
+	void Clear();
+	
 private:
 	Person* head = nullptr;
 
@@ -39,34 +43,44 @@ void main()
 {
 	int numPass, numPpl;
 	PersonList pplList;
-	cout << "Enter amount of passes: ";
-	cin >> numPass;
-	
-	cout << "Enter number of people: ";
-	cin >> numPpl;
-	pplList.InputPpl(numPpl);
-	pplList.HotPotato(numPass, numPpl);
+	//Input and check for errors.
+	// Take input and check for errors. Allow -1 through in case user wants to quit.
+	// 
+	do
+	{
+		cout << "Enter amount of passes: ";
+		cin >> numPass;
+		if (numPass > -1)
+		{
+			cout << "Enter number of people: ";
+			cin >> numPpl;
+			pplList.InputPpl(numPpl);
+			pplList.HotPotato(numPass, numPpl);
+			pplList.Clear();
+		}
+	}
+	while (numPass != -1);
 }
 
 void PersonList::InputPpl(int numPpl)
 {
-	Person* buffer;
-	
 	string name=" ";
 	ifstream inFile;
-	inFile.open("input.txt");
+	inFile.open("input2.txt");
 	//while
 	Person* trav;
 	head=trav = new (nothrow) Person;
-	numPpl--;
-	inFile >> name;
-	trav->name = name;
-
 	if (!trav)
 	{
 		cout << "Ran out of heap memory. Ending program";// CHANGEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 		return;
 	}
+	numPpl--;
+	inFile >> name;
+	trav->name = name;
+	trav->index = 1;
+
+	
 	for (int x = 0; x < numPpl; x++)
 	{
 		trav->next = new (nothrow) Person;
@@ -75,19 +89,64 @@ void PersonList::InputPpl(int numPpl)
 			cout << "Ran out of heap memory. Ending program";// CHANGEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 			return;
 		}
+		trav->next->prev = trav;
 		trav = trav->next;
 		inFile >> name;
 		trav->name = name;
 	}
 	trav->next = head;// circular linked lsit
+	trav->next->prev = trav;
+	inFile.close();///// close infile
 	
 }
 void PersonList::HotPotato(int numPass, int numPpl)
 {
-	Person* trav = head;
-	while (numPpl != 1)
-	{
-		
-	}
+	Person* trav = head;// HEAD will get lost here unless I make it part of the record class(but its a struct)
+	Person* toDelete = trav;
 	
+	int index = 0;
+	while (numPpl != 1)
+	{	
+		
+		if (index == numPass)
+		{
+			index = 0;
+			toDelete = trav;
+			cout << "Player "<< trav->name << " lost the game!" << numPpl << " players left!" << endl;
+			numPpl--;
+			trav->prev->next = trav->next;
+			trav->next->prev = trav->prev;// god this logic send help'
+			trav = trav->next;
+			delete toDelete;
+		}
+		else
+		{
+			index++;
+			trav = trav->next;
+		}
+	}
+	head = trav;
+	cout << trav->name << " has won the game!" << endl;
+}
+void PersonList::Clear() 
+{
+	Person* trav;
+	trav = head;
+	head->prev->next = nullptr;
+	Person* toDelete;
+	while (trav != nullptr)
+	{
+		if (trav->next != nullptr)
+		{
+			toDelete = trav;
+			trav = trav->next;
+			delete toDelete;
+		}
+		else
+		{
+			delete trav;
+			trav = nullptr;
+		}
+	}
+	cout << "List is empty." << endl;
 }
