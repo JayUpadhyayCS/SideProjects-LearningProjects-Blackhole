d67149f2dc1b0cc003f94e85905f77db6d1b72e8
jupadhyay1 <jupadhyay1@yahoo.com>
2020-03-24 19:56:16 -0700

    average in c++ working

--- CS CLASSES/CS230/JXU_LAB2/Source.cpp
+++ CS CLASSES/CS230/JXU_LAB2/Source.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <iomanip>
 using namespace std;
 const int SIZE = 30;
+const double DIVISOR = 0.14748071991788;
 int main()
 {
-
+	string stockSymbols[SIZE];
+	double stockPrices[SIZE];
 	ifstream infile;
+	double total=0;
+	char input;
 	infile.open("C:\\Temp\\Dow30.txt");
 	if (!infile)
 	{
 		cout << "Unable to open C:\\Temp\\Dow30.txt - ending" << endl;
 		exit(-2);
 	}
-	string buffer;
 	for (int x = 0; x < SIZE && !infile.eof();x++)
 	{
-		infile >> buffer;
-		infile >> buffer;
+		infile >> stockSymbols[x];
+		infile >> stockPrices[x];
+		total += stockPrices[x];
 		//ADD TO FIRST ARR
 		//ADD TO SECOND ARR
 		if (x != 30 && infile.eof())
@@ -27,4 +31,9 @@ int main()
 			exit(-2);
 		}
 	}
+	total = total / DIVISOR;
+	cout << "Dow Jones Industrial Average is " << setprecision(2) << fixed << total << "." << endl;
+	cout << "Calculate new value of average?    Reply 'Y' or 'N'." << endl;
+	//cin << input;
+	//if ()
 }
